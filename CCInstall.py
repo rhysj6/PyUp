a2ee0a41c414af21567d1b6d198de8265e2cacc2
rhysj6 <rhysj6@gmail.com>
2018-07-24 08:52:26 +0100

    Update CCInstall.py

--- CCInstall.py
+++ CCInstall.py
@@ -7,7 +7,7 @@ try:
 	f.close()
 except:
 	currentVersion = "Not installed" ##If there is no file then this will fail the if statement
-if webpage != currentVersion: ##Checks if the current version is the same as the latest version 
+if webpage != currentVersion and currentVersion != "Beta" and currentVersion != "Don't Update": ##Checks if the current version is the same as the latest version 
 	print("Installing Custom Code module Version:",webpage)
 	f = open('CCVersion.txt', 'w+')
 	f.write(webpage) ##updates the version number
